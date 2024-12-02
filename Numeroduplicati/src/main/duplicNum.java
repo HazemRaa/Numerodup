63b571e1e978cdcb7210e6ed7c2cbd87ca850fe1
HazemRaa <haz3m95@gmail.com>
2024-12-02 15:07:07 +0100

    Primo commit

---
+++ Numeroduplicati/src/main/duplicNum.java
@@ -0,0 +1,22 @@
+package main;
+public class duplicNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numeri = {4, 3, 2, 7, 8, 2, 3, 1};// Dichiarazione dell'array
+		for(int i = 0; i < numeri.length; i++) { 
+			//il primo for itera su tutti gli elementi dell'arra
+			for( int x = 1+ i; x < numeri.length; x++ ) {
+				// nel secondo for, confronta l'elemento corrente [i] con quelli successivi [x]; 
+				 
+				if( numeri[i]==numeri[x]) //controlla se due numeri sono uguali se true viene stampato
+					System.out.println("numeri Duplicati:" + numeri[i]);
+			}
+			
+		}
+	
+		
+	}
+	
+}
+			
---
+++ Numeroduplicati/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Numeroduplicati {
+}
